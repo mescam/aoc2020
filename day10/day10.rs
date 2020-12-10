a841b84b450a0cf34c8dbee4c142191b40089f07
Jakub Woźniak <jakub.wozniak@nordcloud.com>
2020-12-11 00:40:11 +0100

    Removed unused functions

--- day10/day10.rs
+++ day10/day10.rs
@@ -7,21 +7,6 @@ fn parse(contents: &String) -> Vec<i64> {
     .collect()
 }
 
-fn is_correct(vec: &Vec<i64>) -> bool {
-    match vec.windows(2).find(|arr| arr[1] - arr[0] > 3) {
-        Some(_) => false,
-        None => true
-    }
-}
-
-fn binom(n: i64, k: i64) -> i64 {
-    let mut res = 1;
-    for i in 0..k {
-        res = (res * (n - i)) / (i + 1);
-    }
-    res
-}
-
 fn tribonacci(n: i64) -> i64 {
     match n {
         0 => 1,
@@ -35,11 +20,6 @@ fn diffs(vec: &Vec<i64>, size: usize) -> Vec<i64> {
     vec.windows(size).map(|a| (a[size - 1] - a[0])).collect()
 }
 
-fn binomial_coeff_iterative(input: &Vec<i64>) -> i64 {
-    //[1..(input.len() as i64)]..fold(0, |acc, k| acc + binom(input.len() as i64, k))
-    (1..(input.len() as i64 + 1)).map(|i| binom(input.len() as i64, i)).sum()
-}
-
 fn main() {
     let contents = fs::read_to_string("input.txt")
     .expect("error loading file");
@@ -49,8 +29,6 @@ fn main() {
     adapters.push(0); // outlet jolt
     adapters.push(*adapters.iter().max().unwrap() + 3); // device jolts
     adapters.sort();
-
-    assert_eq!(is_correct(&adapters), true);
 
     let mut ones = 0;
     let mut threes = 0;
